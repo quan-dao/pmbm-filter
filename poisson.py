33030457763e8f6d6aeab63a46236e6fe73655db
Quan Dao <minh-quan.dao@eleves.ec-nantes.fr>
2020-06-19 19:02:21 +0200

    Modify give_birth to make sure 1 meas spawn 1 track

--- poisson.py
+++ poisson.py
@@ -3,6 +3,7 @@ from typing import List
 
 from gaussian_density import State, GaussianDensity
 from object_detection import ObjectDetection
+from target import Target
 
 
 class PointPoissonProcess(object):
@@ -20,6 +21,7 @@ class PointPoissonProcess(object):
         self.prune_threshold = prune_threshold  # in log domain
         self.merge_threshold = merge_threshold  # size of mahalanobis distance to consider 2 gaussian are close
         self.intensity = []  # Dict{w: log_w, s: State}
+        self.target_id_to_give = 0  # birth only comes from PPP, this number defines each target id at its birth
 
     def give_birth(self, measurements: List[ObjectDetection], birth_per_meas=3) -> None:
         """
@@ -31,9 +33,9 @@ class PointPoissonProcess(object):
         :param birth_per_meas: number of birth induced by a measurement
         """
         for meas in measurements:
-            delta_x = np.random.uniform(-5, 5, size=birth_per_meas)
-            delta_y = np.random.uniform(-5, 5, size=birth_per_meas)
-            delta_yaw = np.random.uniform(-np.pi, np.pi, size=birth_per_meas)
+            delta_x = np.random.uniform(-5, 5, size=birth_per_meas).tolist() + [0]  # to ensure each measurement spawns a track
+            delta_y = np.random.uniform(-5, 5, size=birth_per_meas).tolist() + [0]
+            delta_yaw = np.random.uniform(-np.pi, np.pi, size=birth_per_meas).tolist() + [0]
             for i in range(birth_per_meas):
                 mean = np.array([
                     [meas.z[0, 0] + delta_x[i]],  # x
@@ -48,7 +50,7 @@ class PointPoissonProcess(object):
                                        's': State(mean, cov, meas.obj_type, empty_constructor=False)})
 
     def predict(self, measurements: List[ObjectDetection]) -> None:
-        # predict for old components
+        # predict for components already in the intensity
         for component in self.intensity:
             component['w'] += np.log(self.prob_survival)
             component['s'] = self.density_hdl.predict(component['s'])
@@ -56,8 +58,11 @@ class PointPoissonProcess(object):
         self.give_birth(measurements)
 
     def update(self) -> None:
+        """
+        Update for targets that were undetected at the previous time step and stay undetected at the current time step
+        """
         for component in self.intensity:
-            component['w'] += np.log(self.prob_detection)
+            component['w'] += np.log(1 - self.prob_detection)
 
     def prune(self) -> None:
         """
@@ -65,7 +70,7 @@ class PointPoissonProcess(object):
         """
         self.intensity = [component for component in self.intensity if component['w'] > self.prune_threshold]
 
-    def merge(self) ->None:
+    def merge(self) -> None:
         """
         Reduce the intensity by merging the "close" components  (close in the sense of Mahalanobis distance)
         """
@@ -73,4 +78,10 @@ class PointPoissonProcess(object):
         states = [component['s'] for component in self.intensity]
         reduced_log_weights, reduced_states = self.density_hdl.mixture_reduction(log_weights, states, self.merge_threshold)
         self.intensity = [{'w': log_w, 's': state} for log_w, state in zip(reduced_log_weights, reduced_states)]
+
+    def create_new_targets(self, measurements: List[ObjectDetection]) -> List[Target]:
+        """
+        TODO: Perform update for targets detected for the first time
+        :return: a list new tracks spawned by this list of measurements
+        """
 
